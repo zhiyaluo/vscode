38fc60e65eccc29b9c6c7e85f9e2261718ecdc1b
Miguel Solorio <miguel.solorio@microsoft.com>
2019-02-20 08:14:10 -0800

    Add tree icons and create method for setting data

--- src/vs/workbench/services/themes/browser/workbenchThemeService.ts
+++ src/vs/workbench/services/themes/browser/workbenchThemeService.ts
@@ -506,7 +506,7 @@ export class WorkbenchThemeService implements IWorkbenchThemeService {
 	private doSetFileIconTheme(iconThemeData: FileIconThemeData): void {
 		this.currentIconTheme = iconThemeData;
 
-		document.body.dataset.iconTheme = iconThemeData.settingsId;
+		this.addData(iconThemeData);
 
 		if (this.container) {
 			if (iconThemeData.id) {
@@ -532,6 +532,10 @@ export class WorkbenchThemeService implements IWorkbenchThemeService {
 		}
 		this.onFileIconThemeChange.fire(this.currentIconTheme);
 
+	}
+
+	private addData(iconThemeData: FileIconThemeData) {
+		document.body.dataset.iconTheme = iconThemeData.settingsId;
 	}
 
 	private writeFileIconConfiguration(settingsTarget: ConfigurationTarget | undefined | 'auto'): Promise<IFileIconTheme> {
