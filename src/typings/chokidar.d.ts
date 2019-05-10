767a6b354a85af60c170ad0924c105e7ece86d37
deepak1556 <hop2deep@gmail.com>
2019-05-09 18:09:26 -0700

    FIXME: update build to work with node12

--- src/typings/chokidar.d.ts
+++ src/typings/chokidar.d.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-declare module 'vscode-chokidar' {
+declare module 'chokidar' {
 
 	/**
 	 *  takes paths to be watched recursively and options
